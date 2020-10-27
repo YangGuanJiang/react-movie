3bfcd8255cf05427ce5f857a7c8dad56d04f3d99
guanjiang Yang <guanjiang.yang@gmail.com>
2020-10-27 01:02:06 -0400

    Updates

---
+++ precache-manifest.bffb9f7894a6a33b353c8c769aaa4cdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5be14705ffb30089909d374516f38fbd",
+    "url": "/react-movie/index.html"
+  },
+  {
+    "revision": "0f7ad10ccf1d0c9a2adb",
+    "url": "/react-movie/static/css/main.c18d63e7.chunk.css"
+  },
+  {
+    "revision": "d63d24631199f7d6e69f",
+    "url": "/react-movie/static/js/2.cead2c7a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-movie/static/js/2.cead2c7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f7ad10ccf1d0c9a2adb",
+    "url": "/react-movie/static/js/main.4fda7fea.chunk.js"
+  },
+  {
+    "revision": "70ff08b51119de3fac99",
+    "url": "/react-movie/static/js/runtime-main.b5afdfdf.js"
+  }
+]);
